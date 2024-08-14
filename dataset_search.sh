e439e3842e1583d1ceab9fc26b3d7a4a289ae1b4
zichunhao <zichunh2000@gmail.com>
2024-08-14 18:47:26 +0200

    Positional arguments

--- dataset_search.sh
+++ dataset_search.sh
@@ -1,4 +1,7 @@
-# dataset=$1
-dataset="/GluGlutoHHto4B_kl-1p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer23MiniAODv4-130X_mcRun3_2023_realistic_v15-v3/MINIAODSIM"
-dasgoclient --query="file dataset=$dataset" | tee dataset.txt
+dataset=$1
+job_dir=$2 # optional
+if [ -z "$job_dir" ]; then
+    job_dir="."
+fi
+dasgoclient --query="file dataset=$dataset" | tee ${job_dir}/dataset.txt
 echo "Dataset $dataset has `wc -l dataset.txt` files"
