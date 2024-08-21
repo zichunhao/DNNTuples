c9fdb574e630616f0b591feb69861ea3fb512463
zichunhao <zichunh2000@gmail.com>
2024-08-21 21:36:02 +0200

    Use create.py to generate condor jobs

---
+++ dnntuple_tt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+# from dasgoclient --query='dataset=/TTto4Q_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM'
+DATASETS=(
+    "TTto4Q_TuneCP5_13p6TeV_powheg-pythia8"
+)
+for DATASET in "${DATASETS[@]}"; do
+    python3 create.py --dataset "/"${DATASET}/"Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" \
+    --eos-path "root://redirector.t2.ucsd.edu:1095//store/user/zichun/Hbb-tagger/samples/${DATASET}" \
+    --job-tag ${DATASET} \
+    --max-retries 10;
+    
+    condor_submit ${DATASET}/dnntuple.jdl
+done
