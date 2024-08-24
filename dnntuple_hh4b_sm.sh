bda7ee977e36f7a4618ad315e1f1aede063c62c6
zichunhao <zichunh2000@gmail.com>
2024-08-24 23:56:44 +0200

    Update dest path

--- dnntuple_hh4b_sm.sh
+++ dnntuple_hh4b_sm.sh
@@ -5,7 +5,7 @@ DATASETS=(
 )
 for DATASET in "${DATASETS[@]}"; do
     python3 create.py --dataset "/"${DATASET}/"Run3Summer23MiniAODv4-130X_mcRun3_2023_realistic_v15-v3/MINIAODSIM" \
-    --eos-path "root://redirector.t2.ucsd.edu:1095//store/user/zichun/Hbb-tagger/samples/${DATASET}" \
+    --eos-path "root://redirector.t2.ucsd.edu:1095//store/user/zichun/Hbb-tagger/samples/${DATASET}/dnntuples" \
     --job-tag ${DATASET} \
     --max-retries 10;
     
--- dnntuple_tt.sh
+++ dnntuple_tt.sh
@@ -5,7 +5,7 @@ DATASETS=(
 )
 for DATASET in "${DATASETS[@]}"; do
     python3 create.py --dataset "/"${DATASET}/"Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2/MINIAODSIM" \
-    --eos-path "root://redirector.t2.ucsd.edu:1095//store/user/zichun/Hbb-tagger/samples/${DATASET}" \
+    --eos-path "root://redirector.t2.ucsd.edu:1095//store/user/zichun/Hbb-tagger/samples/${DATASET}/dnntuples" \
     --job-tag ${DATASET} \
     --max-retries 10;
     
