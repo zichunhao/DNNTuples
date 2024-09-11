2871d93b38c74d39c523c42bd86a122a1923cb0b
zichunhao <zichunh2000@gmail.com>
2024-09-11 22:09:22 +0200

    Add HH4b SM samples in 2022

--- dnntuple_hh4b_sm.sh
+++ dnntuple_hh4b_sm.sh
@@ -2,6 +2,7 @@
 # GluGlutoHHto4B_kl-1p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer23MiniAODv4-130X_mcRun3_2023_realistic_v15-v3/MINIAODSIM
 DATASETS=(
     "GluGlutoHHto4B_kl-1p00_kt-1p00_c2-0p00_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer23MiniAODv4-130X_mcRun3_2023_realistic_v15-v3"
+    "GluGlutoHHto4B_kl-1p00_kt-1p00_c2-0p00_LHEweights_TuneCP5_13p6TeV_powheg-pythia8/Run3Summer22MiniAODv4-130X_mcRun3_2022_realistic_v5-v2"
 )
 for DATASET in "${DATASETS[@]}"; do
     python3 create.py --dataset "/"${DATASET}/"MINIAODSIM" \
@@ -9,5 +10,5 @@ for DATASET in "${DATASETS[@]}"; do
     --job-tag ${DATASET} \
     --max-retries 10;
     
-    # condor_submit condor_jobs/${DATASET}/dnntuple.jdl
+    condor_submit condor_jobs/${DATASET}/dnntuple.jdl
 done
